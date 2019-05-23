795b06fb1365c7d985e0db9683549cacf9347b7a
Stephanie Burcombe <sburcombe@gmail.com>
2019-05-23 16:49:16 -0700

    Made two cards flip at different times

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-var hello;
+$(document).ready(initializeApp);
+
+function initializeApp() {
+
+    $(".card").click(handleCardClick);
+
+
+}
+
+
+function handleCardClick(event) {
+
+     $(this).find("div.front-card").addClass("hidden");
+     //find a child inside of an element in jQuery
+
+    console.log("You clicked the card!");
+}
